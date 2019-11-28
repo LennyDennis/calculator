09e020b258ba5b3791a029383d5fc6aea0eb5adb
LennyDennis <lennydennis@gmail.com>
2019-11-28 12:24:23 +0300

    This a calculator that gets user input through js prompts

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+function add(num1, num2){
+    return num1+num2;
+};
+var num1 = parseInt(prompt('Enter the 1st number'));
+var num2 = parseInt(prompt('Enter the 2nd number'));
+alert(add(num1,num2));
+
+function multiply(num1, num2){
+    return num1*num2;
+};
+var num1 = parseInt(prompt('Enter the 1st number'));
+var num2 = parseInt(prompt('Enter the 2nd number'));
+alert(multiply(num1,num2));
+function subtraction(num1, num2){
+    return num1-num2;
+};
+var num1 = parseInt(prompt('Enter the 1st number'));
+var num2 = parseInt(prompt('Enter the 2nd number'));
+alert(subtraction(num1,num2));
+function division(num1, num2){
+    return num1/num2;
+};
+var num1 = parseInt(prompt('Enter the 1st number'));
+var num2 = parseInt(prompt('Enter the 2nd number'));
+alert(division(num1,num2));
